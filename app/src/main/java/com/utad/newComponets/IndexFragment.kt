9e201b1faa75a036b020b3625abf346004d36af7
martamagui <73697174+martamagui@users.noreply.github.com>
2023-07-20 13:26:05 +0200

    Index Navigation

--- app/src/main/java/com/utad/newComponets/IndexFragment.kt
+++ app/src/main/java/com/utad/newComponets/IndexFragment.kt
@@ -26,6 +26,60 @@ class IndexFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.btnGoToSwitch.setOnClickListener { goToSwitchFragment() }
+        binding.btnGoToCheckbox.setOnClickListener { goToCheckBox() }
+        binding.btnGoToButtons.setOnClickListener { goToButtonsFragment() }
+        binding.btnGoToFab.setOnClickListener { gotToFabFragment() } 
+        binding.btnGoToCardView.setOnClickListener { goToCardViewFragment() }
+        binding.btnGoToScrolls.setOnClickListener { goToScrollFragment() }
+        binding.btnGoToToolbar.setOnClickListener { goToToolbarFragment() }
+        binding.btnGoToCoordinator.setOnClickListener { goToCoordinatorFragment() }
+        binding.btnGoToBottomNavigation.setOnClickListener { goToBottomNavigationFragment() }
+        binding.btnGoToNavDrawer.setOnClickListener { goToNavdrawerFragment() }
+    }
+
+    private fun goToNavdrawerFragment() {
+        val action = IndexFragmentDirections.actionIndexFragmentToNavdrawerFragment()
+        findNavController().navigate(action)
+    }
+
+    private fun goToBottomNavigationFragment() {
+        val action = IndexFragmentDirections.actionIndexFragmentToBottomNavigationFragment()
+        findNavController().navigate(action)
+    }
+
+    private fun goToCoordinatorFragment() {
+        val action = IndexFragmentDirections.actionIndexFragmentToCoordinatorAndCollapsingToolbarFragment()
+        findNavController().navigate(action)
+    }
+
+    private fun goToToolbarFragment() {
+        val action = IndexFragmentDirections.actionIndexFragmentToToolbarFragment()
+        findNavController().navigate(action)
+    }
+
+    private fun goToScrollFragment() {
+        val action = IndexFragmentDirections.actionIndexFragmentToScrollsFragment()
+        findNavController().navigate(action)
+    }
+
+    private fun goToCardViewFragment() {
+        val action = IndexFragmentDirections.actionIndexFragmentToCardViewFragment()
+        findNavController().navigate(action)
+    }
+
+    private fun gotToFabFragment() {
+        val action = IndexFragmentDirections.actionIndexFragmentToFabFragment()
+        findNavController().navigate(action)
+    }
+
+    private fun goToButtonsFragment() {
+        val action = IndexFragmentDirections.actionIndexFragmentToButtonsFragment()
+        findNavController().navigate(action)
+    }
+
+    private fun goToCheckBox() {
+        val action = IndexFragmentDirections.actionIndexFragmentToSwitchFragment()
+        findNavController().navigate(action)
     }
 
     private fun goToSwitchFragment() {
---
+++ app/src/main/java/com/utad/newComponets/materialComponents/FabFragment.kt
@@ -0,0 +1,28 @@
+package com.utad.newComponets.materialComponents
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.utad.newComponets.R
+import com.utad.newComponets.databinding.FragmentFabBinding
+
+
+class FabFragment : Fragment() {
+    private lateinit var _binding: FragmentFabBinding
+    private val binding: FragmentFabBinding get() = _binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentFabBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
