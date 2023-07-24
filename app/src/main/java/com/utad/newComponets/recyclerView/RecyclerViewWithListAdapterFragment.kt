9ef61bec235ac464d8f86e79717c838f42ae679a
martamagui <73697174+martamagui@users.noreply.github.com>
2023-07-24 22:43:17 +0200

    WIP - Created Fragment for ListAdapter

---
+++ app/src/main/java/com/utad/newComponets/recyclerView/RecyclerViewWithListAdapterFragment.kt
@@ -0,0 +1,23 @@
+package com.utad.newComponets.recyclerView
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.utad.newComponets.R
+
+
+class RecyclerViewWithListAdapterFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_recycler_view_with_list_adapter, container, false)
+    }
+
+
+}
