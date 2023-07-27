be8bebf15280a18d4bad3a8f6c3184190b34ac8c
martamagui <73697174+martamagui@users.noreply.github.com>
2023-07-27 22:49:54 +0200

    ContextMenu

--- app/src/main/java/com/utad/newComponets/components/ContextMenuActivity.kt
+++ app/src/main/java/com/utad/newComponets/components/ContextMenuActivity.kt
@@ -19,40 +19,58 @@ class ContextMenuActivity : AppCompatActivity() {
         _binding = ActivityContextMenuBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        registerForContextMenu(binding.cvContextMenu)
+        //Ponemos en observación la vista en la que queremos que aparezca el ContextMenu
+        registerForContextMenu(binding.cvDogMenu)
 
         binding.btnShow.setOnClickListener {
-            binding.cvContextMenu.visibility = View.VISIBLE
-            binding.btnShow.visibility = View.GONE
+            showCard()
         }
     }
 
+    //Para asignar nuestro menu al contextMenú cuando se cree
     override fun onCreateContextMenu(
         menu: ContextMenu?,
         v: View?,
         menuInfo: ContextMenu.ContextMenuInfo?
     ) {
         super.onCreateContextMenu(menu, v, menuInfo)
+        //obtenemos el layoutinflater del menu
         val menuInflater = menuInflater
+        //hacemos que el inflater cargue nuestro menu
         menuInflater.inflate(R.menu.menu_example_context_menu, menu)
     }
 
+    //Nos permite controlar el item que ha sido pulsado
     override fun onContextItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.hide) {
-            binding.cvContextMenu.visibility = View.GONE
-            binding.btnShow.visibility = View.VISIBLE
+            hideCard()
         } else if (item.itemId == R.id.change_color) {
-            val colors = listOf(
-                R.color.green,
-                R.color.gray,
-                R.color.md_theme_dark_onPrimary,
-                R.color.md_theme_dark_onSurface,
-                R.color.md_theme_dark_surfaceTint,
-                R.color.md_theme_dark_tertiaryContainer,
-                R.color.CustomColor1
-            )
-            binding.cvContextMenu.setCardBackgroundColor(getColor(colors.random()))
+            changeCardBgColor()
         }
+
         return super.onContextItemSelected(item)
+    }
+
+    private fun hideCard() {
+        binding.cvDogMenu.visibility = View.GONE
+        binding.btnShow.visibility = View.VISIBLE
+    }
+
+    private fun showCard() {
+        binding.cvDogMenu.visibility = View.VISIBLE
+        binding.btnShow.visibility = View.GONE
+    }
+
+    private fun changeCardBgColor() {
+        val colors = listOf(
+            R.color.green,
+            R.color.gray,
+            R.color.md_theme_dark_onPrimary,
+            R.color.md_theme_dark_onSurface,
+            R.color.md_theme_dark_surfaceTint,
+            R.color.md_theme_dark_tertiaryContainer,
+            R.color.CustomColor1
+        )
+        binding.cvDogMenu.setCardBackgroundColor(getColor(colors.random()))
     }
 }
